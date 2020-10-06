77b6320ab15b31e9586e5c32fe494a7f2697361f
swebb1 <swebb1@staffmail.ed.ac.uk>
2020-10-07 00:01:46 +0100

    build site

--- _site.R
+++ _site.R
@@ -1,13 +1,13 @@
 ## packages I want loaded for all pages of my site
-suppressPackageStartupMessages({
-  library(tidyverse)
-  library(stringr)
-})
+#suppressPackageStartupMessages({
+#  library(tidyverse)
+#  library(stringr)
+#})
 
 ## variables I need for my site 
-data <- readr::read_csv('important_data.csv')
+#data <- readr::read_csv('important_data.csv')
 
 
 ## knitr options I want set as default for all ('global') code chunks
-knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
+#knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
 
